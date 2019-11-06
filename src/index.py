d665970e5e5263b72406b97264d678633a05748c
Javier <topnaita@gmail.com>
2019-11-06 16:47:15 -0400

    Using for {%%}

--- src/index.py
+++ src/index.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+post = [
+    {
+        'author' : 'javier',
+        'title' : 'Python tutorial',
+        'content' : 'new york',
+        'date_posted' : 'april 22 2019'    
+
+
+    },
+    {
+        'author' : 'jean',
+        'title' : 'Flask framework',
+        'content' : 'france',
+        'date_posted' : 'april 22 2019'
+
+
+    }
+
+
+]
+
+
 @app.route('/')
 def home(): 
-    return render_template('home.html')
+    return render_template('home.html', post=post)
 
 @app.route('/about')
 def about(): 
